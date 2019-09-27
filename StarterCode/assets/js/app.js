fadb802aace7ba55cd826cfe5ce8e2c43145149a
DwightMauk <dwightmauk@gmail.com>
2019-09-27 16:31:20 -0500

    final change

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,143 @@
 // @TODO: YOUR CODE HERE!
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
+
+  // if the SVG area isn't empty when the browser loads,
+  // remove it and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
+
+  // clear svg is not empty
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+ 
+  // SVG wrapper dimensions are determined by the current width and
+  // height of the browser window.
+  var svgWidth = window.innerWidth;
+  var svgHeight = window.innerHeight;
+
+  var margin = {
+    top: 50,
+    bottom: 50,
+    right: 50,
+    left: 50
+  };
+
+  var height = svgHeight - margin.top - margin.bottom;
+  var width = svgWidth - margin.left - margin.right;
+
+  // Append SVG element
+  var svg = d3
+    .select(".scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+  // Append group element
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    
+
+  // Read CSV
+  d3.csv('assets/data/data.csv').then(function(fullData) {
+    fullData.forEach(function(data) {
+    data.healthcare = +data.healthcare;
+    console.log(data.healthcare);
+    //if (!data.healthcare) {data.healthcare = 0};
+    data.obesity = +data.obesity;
+    //console.log(fullData);
+  });
+
+labelArea = 30;
+
+    
+      // create scales
+      var xLinearScale = d3.scaleLinear()
+        .domain(d3.extent(fullData, d => d.obesity))
+        .range([margin.left + labelArea, width - margin.right]);
+
+      var yLinearScale = d3.scaleLinear()
+        .domain(d3.extent(fullData, d => d.healthcare))
+        .range([height- margin.top - labelArea, margin.bottom]);
+
+      // create axes
+      var xAxis = d3.axisBottom(xLinearScale);
+      var yAxis = d3.axisLeft(yLinearScale);
+
+      
+      
+
+      // append circles
+      var circles = 
+      svg
+        .selectAll("circle")
+        .data(fullData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.obesity))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", 15)
+        .attr("class", "stateCircle");
+        circles
+        .selectAll("text")
+        .data(fullData)
+        .enter()
+        .append("text")
+        .attr("class","stateText")
+         .attr("dx", d => xLinearScale(d.obesity))
+         .attr("dy", d => yLinearScale(d.healthcare))
+         .attr("font-size",15)
+         .text(function(d) {
+          return d.abbr;});
+
+svg.append("g")
+        .call(xAxis)
+        .attr("transform","translate(0,"+ (height-margin.bottom-labelArea) + ")");
+
+        svg.append("g")
+        .call(yAxis)
+        .attr("transform","translate("+ (margin.left+labelArea) + ", 0)");
+
+  
+      // Step 1: Initialize Tooltip
+      var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(fullData) {
+          return (`<strong>State: ${d.state}<strong><hr>Obesity: ${d.obesity}<strong><hr>Health Care: ${d.healthcare}`);
+        });
+       
+      // Step 2: Create the tooltip in chartGroup.
+      chartGroup.call(toolTip);
+
+      // Step 3: Create "mouseover" event listener to display tooltip
+      chartGroup.on("mouseover", function(d) {
+        toolTip.show(d, this);
+      })
+      // Step 4: Create "mouseout" event listener to hide tooltip
+        .on("mouseout", function(d) {
+          toolTip.hide(d);
+        });
+      });
+      chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks Health Care %");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.bottom - 120})`)
+      .attr("class", "axisText")
+      .text("Obesity %");
+    };
+
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
